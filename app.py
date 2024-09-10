162112ab086aeb337ebde4fe01e2b21fb8bb4fd9
shujjad ali <shujatali683@gmail.com>
2024-09-10 20:18:10 +0530

    added bar and scatter plot feature

--- app.py
+++ app.py
@@ -4,36 +4,76 @@ import pandas as pd
 import plotly.express as px 
 
 st.set_page_config(layout='wide')
-df=pd.read_csv('india.csv')
+
+# Load the data
+df = pd.read_csv('india.csv')
 
 list_of_states=list(df['State'].unique())
 list_of_states.insert(0,'Overall India')
 
+
 st.sidebar.title('India Viualisation')
 
-selected_state=st.sidebar.selectbox('Select State',list_of_states)
 
-primary=st.sidebar.selectbox('select Primary Parameter',sorted(list(df.columns[5:6]) + list(df.columns[13:16]) + list(df.columns[18:])))
-secondary=st.sidebar.selectbox('select secondary Parameter',sorted(list(df.columns[5:6]) + list(df.columns[13:16]) + list(df.columns[18:])))
-
-
-plot=st.sidebar.button('plot Graph')
-
+selected_state = st.sidebar.selectbox('Select State', list_of_states)
+primary = st.sidebar.selectbox('Select Primary Parameter', sorted(list(df.columns[5:6]) + list(df.columns[13:16]) + list(df.columns[18:])))
+secondary = st.sidebar.selectbox('Select Secondary Parameter', sorted(list(df.columns[5:6]) + list(df.columns[13:16]) + list(df.columns[18:])))
+plot = st.sidebar.button('Plot Graph')
 if plot:
     st.text('Size represents primary parameter')
-    st.text('color represents secondary parameter')
-    
-
+    st.text('Color represents secondary parameter')
     if selected_state == 'Overall India':
-        #plot for india
-        fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=4,size_max=35,
-                                mapbox_style="carto-positron",width=1200,height=700,hover_name='District')
+        fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=4, size_max=35,
+                                mapbox_style="carto-positron", width=1200, height=700, hover_name='District')
+    else:
+        state_df = df[df['State'] == selected_state]
+        fig = px.scatter_mapbox(state_df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=4, size_max=35,
+                                mapbox_style="carto-positron", width=1200, height=700, hover_name='District')
+    
+    st.plotly_chart(fig)
+         
+def india_bar_chart():
+    st.title("India Visualization App")
+    
+    st.sidebar.title('Bar Chart')
+    list_of_states = list(df['State'].unique())
+    list_of_states.insert(0, 'Overall India')
+
+    selected_state = st.sidebar.selectbox('Select State', list_of_states)
+    
+    x_axis = st.sidebar.selectbox('Select X-axis Parameter', sorted(list(df.columns[5:6]) + list(df.columns[13:16]) + list(df.columns[18:])))
+    y_axis = st.sidebar.selectbox('Select Y-axis Parameter', sorted(list(df.columns[5:6]) + list(df.columns[13:16]) + list(df.columns[18:])))
+    plot = st.sidebar.button('Plot Graph')
+
+    if plot:
+
+        if selected_state == 'Overall India':
+            fig = px.bar(df,x=x_axis,y=y_axis, width=1200, height=700,hover_name='State')
+        else:
+            state_df = df[df['State'] == selected_state]
+            fig = px.bar(state_df,x=x_axis,y=y_axis,width=1200, height=700, hover_name='District')
         
         st.plotly_chart(fig)
-    else:
-        #plot for state
-        state_df=df[df['State']==selected_state]
 
-        fig = px.scatter_mapbox(state_df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=4,size_max=35,
-                                mapbox_style="carto-positron",width=1200,height=700,hover_name='District')
+def india_scatter_plot():
+    st.title("India Visualization App")
+    
+    st.sidebar.title('Scatter Plot')
+    list_of_states = list(df['State'].unique())
+    list_of_states.insert(0, 'Overall India')
+
+    selected_state = st.sidebar.selectbox('Select State', list_of_states)
+    
+    x_axis = st.sidebar.selectbox('Select X-axis Parameter', sorted(list(df.columns[5:6]) + list(df.columns[13:16]) + list(df.columns[18:])))
+    y_axis = st.sidebar.selectbox('Select Y-axis Parameter', sorted(list(df.columns[5:6]) + list(df.columns[13:16]) + list(df.columns[18:])))
+    plot = st.sidebar.button('Plot Graph')
+
+    if plot:
+
+        if selected_state == 'Overall India':
+            fig = px.scatter(df,x=x_axis,y=y_axis, width=1200, height=700, color="State",hover_name='State')
+        else:
+            state_df = df[df['State'] == selected_state]
+            fig = px.scatter(state_df,x=x_axis,y=y_axis,width=1200, height=700, hover_name='District')
+        
         st.plotly_chart(fig)
